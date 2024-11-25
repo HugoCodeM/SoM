5dd42471cbc8fb82399c315b4d0bbdf3d080c332
HugoCodeM <hugohmignon@gmail.com>
2024-11-25 12:34:42 -0500

    Practice Problems

---
+++ java101/PracticeProblems.java
@@ -0,0 +1,58 @@
+public class PracticeProblems {
+    // Practice Problem 1
+    public static int countOccurrences(int[] arr, int n) {
+        int count = 0;  
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == n) {  
+                count++;  
+            }
+        }
+        return count;
+    }
+    // Practice Problem 2
+    public static int[] reverseArray(int[] arr) {
+        int n = arr.length;  
+        int[] reversedArr = new int[n];  
+        for (int i = 0; i < n; i++) {
+            reversedArr[i] = arr[n - 1 - i]; 
+        }
+
+        return reversedArr;  
+    }
+    // Practice Problem 3
+     public static double sumGrid(double[][] grid) {
+        double sum = 0;  
+
+     
+        for (int i = 0; i < grid.length; i++) {
+         
+            for (int j = 0; j < grid[i].length; j++) {
+                sum += grid[i][j]; 
+            }
+        }
+
+        return sum;  
+    }
+    // Practice Problem 4
+    public static int fib(int n) {
+        if (n == 0) {
+            return 0;  
+        } else if (n == 1) {
+            return 1;  
+        } else {
+            
+            int a = 0, b = 1;
+            for (int i = 2; i <= n; i++) {
+                int next = a + b;  
+                a = b;  
+                b = next;  
+            }
+            return b;  
+        }
+    }
+}    
+        
+
+
+
---
+++ java101/README.md
@@ -0,0 +1,1 @@
+[java101](https://github.com/SciBorgs/SciGuides/blob/main/projects/intro-to-programming/Java101.md)
